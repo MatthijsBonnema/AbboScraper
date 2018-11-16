0aced45863edcd94cd915727d8fe8d4c66f0f307
Matthijs Bonnema <msbonnema@gmail.com>
2018-11-16 13:33:46 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-from selenium.common.exceptions import TimeoutException, NoSuchElementException, StaleElementReferenceException
+from selenium.common.exceptions import TimeoutException, NoSuchElementException, StaleElementReferenceException, ElementNotVisibleException
 from re import sub
 from time import sleep
 import ftplib
@@ -37,8 +37,7 @@ def cookie_clicker(driver, link):
 
 def get_phones(driver):
     page = 1
-    link = "https://telefoonabonnementen.mediamarkt.nl/smartphone-plus-abonnement/merk/samsung?sort=popular&p={}".format(
-        page)
+    link = ""
 
     phones = []
 
@@ -66,8 +65,7 @@ def get_phones(driver):
                 phones.append(phoneLink)
 
         page += 1
-        link = "{}".format(
-            page)
+        link = ""
 
     return phones
 
@@ -88,7 +86,6 @@ def get_phone_info(link, driver):
     test = open("test.txt", 'w')
     test.write(phone_name)
 
-
     phone_dict = {"name": phone_name}
     providers = driver.find_elements_by_class_name("provider-border")
 
@@ -98,19 +95,57 @@ def get_phone_info(link, driver):
         provider_name = provider.get_attribute("title")
         provider.click()
         sleep(0.4)
-        try:
-            total_credit = driver.find_element_by_class_name("credit-table-totalvalue").text.strip("€").strip(",-")
-        except NoSuchElementException:
-            total_credit = "0"
-            pass
-        except StaleElementReferenceException:
-            total_credit = "0"
-            pass
 
-        test.write("{}      {}".format(provider_name, total_credit))
+        options = driver.find_elements_by_class_name("form-option-dropdown")
+        if len(options) > 0:
+            option_nr = 0
+            for option in options:
+                try:
+                    option.click()
+                except StaleElementReferenceException:
+                    sleep(0.2)
+                    option.click()
+                sleep(0.4)
 
-        phone_dict["{}".format(provider_name)] = total_credit
-        print(phone_name, provider_name, total_credit)
+                try:
+                    two_years = driver.find_element_by_xpath("//input[@type='radio' and @value='24']/following-sibling::label")
+                    two_years.click()
+                except ElementNotVisibleException:
+                    pass
+
+                try:
+                    total_credit = driver.find_element_by_class_name("credit-table-totalvalue").text.strip(
+                        "€").strip(",-")
+                except NoSuchElementException:
+                    total_credit = "0"
+                    pass
+                except StaleElementReferenceException:
+                    total_credit = "0"
+                    pass
+                if option_nr == 0:
+                    phone_dict["{}".format("{}".format(provider_name))] = total_credit
+                    print(phone_name, provider_name, total_credit)
+                elif option_nr == 1:
+                    phone_dict["{}".format("{}_secundair".format(provider_name))] = total_credit
+                    print(phone_name, "{}".format("{}_secundair".format(provider_name)), total_credit)
+                else:
+                    print("Dafuq!")
+                option_nr += 1
+        else:
+            try:
+                total_credit = driver.find_element_by_class_name("credit-table-totalvalue").text.strip("€").strip(",-")
+            except NoSuchElementException:
+                total_credit = "0"
+                pass
+            except StaleElementReferenceException:
+                total_credit = "0"
+                pass
+            phone_dict["{}".format(provider_name)] = total_credit
+            total_credit_secundair = "-"
+            phone_dict["{}".format("{}_secundair".format(provider_name))] = total_credit_secundair
+
+            print(phone_name, provider_name, total_credit)
+
     test.close()
     return phone_dict
 
@@ -130,7 +165,6 @@ def create_page(phones_info):
 
     phones_info = sorted(phones_info, key=lambda k: k['name'], reverse=True)
 
-
     for phone in phones_info:
         if phone_nr % 2 == 0:
             table_class = "tg-even"
@@ -138,19 +172,31 @@ def create_page(phones_info):
             table_class = "tg-odd"
         result.write("    <tr>\n")
         provider_lijst = ['Vodafone', 'T-Mobile', 'Tele2', 'KPN', 'Telfort', 'Ben', 'Hollandsnieuwe', 'Lebara']
+        provider_lijst_2 = ['Vodafone', 'T-Mobile', 'Tele2', 'KPN', 'Lebara']
         result.write('        <td class="{}">{}</td>\n'.format(table_class, phone['name']))
 
         for provider in provider_lijst:
             try:
                 result.write('        <td class="{}">&euro; {}</td>\n'.format(table_class, phone[provider]))
+                if provider in provider_lijst_2:
+                    result.write('        <td class="{}">&euro; {}</td>\n'.format(table_class, phone["{}_secundair".format(provider)]))
             except KeyError:
-                result.write('        <td class="{}">&euro; {}</td>\n'.format(table_class, "-"))
+                try:
+                    result.write('        <td class="{}">&euro; {}</td>\n'.format(table_class, phone[provider]))
+                    if provider in provider_lijst_2:
+                        result.write('        <td class="{}">&euro; -</td>\n'.format(table_class))
+
+                except KeyError:
+                    result.write('        <td class="{}">&euro; -</td>\n'.format(table_class))
+
 
         result.write("    </tr>\n")
 
         phone_nr += 1
 
+    result.write("</tbody>\n")
     result.write("</table>\n")
+    result.write("<p><h3>Gemaakt door Matthijs Bonnema, voor vragen mail admin[apenstaartje]msbonnema.nl<h3>\n")
 
 
 def upload_page(filename):
@@ -164,7 +210,7 @@ def upload_page(filename):
 
 
 if __name__ == '__main__':
-    os.chdir("C:\\Users\\Matthijs\\Projects\\AbboScraper")
+    os.chdir("")
     chrome_options = Options()
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-gpu")
@@ -175,11 +221,11 @@ if __name__ == '__main__':
         driver = webdriver.Chrome(options=chrome_options, executable_path=chrome_driver)
         driver.maximize_window()
         driver.get("https://google.nl")
-        phones = get_phones(driver)        phones_info = []
+        phones = get_phones(driver)
+        phones_info = []
         for phone in phones:
             phones_info.append(get_phone_info(phone, driver))
-        # phones_info = [{'name': 'Samsung Galaxy S9 Zwart', 'Vodafone': '480', 'T-Mobile': '480', 'Tele2': '480', 'KPN': '528', 'Telfort': '528', 'Hollandsnieuwe': '528', 'Lebara': '516,40'}, {'name': 'Samsung Galaxy S9+ Zwart', 'Vodafone': '552', 'T-Mobile': '576', 'Tele2': '576', 'KPN': '600', 'Telfort': '624', 'Hollandsnieuwe': '624', 'Lebara': '612,93'}, {'name': 'Samsung Galaxy Note9 128GB Midnight Zwart', 'Vodafone': '768', 'T-Mobile': '720', 'Tele2': '768', 'KPN': '720', 'Telfort': '816', 'Hollandsnieuwe': '816', 'Lebara': '821,75'}, {'name': 'Samsung Galaxy S8 Zwart', 'Vodafone': '360', 'T-Mobile': '384', 'Tele2': '336', 'KPN': '408', 'Telfort': '480', 'Ben': '468', 'Hollandsnieuwe': '432', 'Lebara': '468,72'}, {'name': 'Samsung Galaxy A6 2018 Paars', 'Vodafone': '192', 'T-Mobile': '168', 'Tele2': '168', 'KPN': '144', 'Telfort': '192', 'Ben': '216', 'Hollandsnieuwe': '216', 'Lebara': '218,59'}, {'name': 'Samsung Galaxy A8 Dual Sim Zwart', 'Vodafone': '288', 'T-Mobile': '216', 'Tele2': '216', 'KPN': '216', 'Telfort': '240', 'Ben': '300', 'Hollandsnieuwe': '288', 'Lebara': '299'}, {'name': 'Samsung Galaxy A7 64GB Blauw', 'Vodafone': '192', 'T-Mobile': '192', 'Tele2': '192', 'KPN': '240', 'Telfort': '264', 'Ben': '264', 'Hollandsnieuwe': '216', 'Lebara': '258,13'}, {'name': 'Samsung Galaxy A6+ 2018 Paars', 'Vodafone': '216', 'T-Mobile': '192', 'Tele2': '192', 'KPN': '192', 'Telfort': '240', 'Ben': '240', 'Hollandsnieuwe': '240', 'Lebara': '245,06'}, {'name': 'Samsung Galaxy J6 2018 Goud', 'Vodafone': '120', 'T-Mobile': '96', 'Tele2': '96', 'KPN': '72', 'Telfort': '144', 'Ben': '156', 'Hollandsnieuwe': '144', 'Lebara': '153,68'}, {'name': 'Samsung Galaxy A5 2017 Zwart', 'Vodafone': '240', 'T-Mobile': '240', 'Tele2': '192', 'KPN': '240', 'Telfort': '192', 'Ben': '192', 'Hollandsnieuwe': '240', 'Lebara': '231,62'}, {'name': 'Samsung Galaxy S7 Zwart', 'Vodafone': '336', 'T-Mobile': '288', 'Tele2': '288', 'KPN': '336', 'Telfort': '336', 'Ben': '324', 'Hollandsnieuwe': '336', 'Lebara': '326,88'}, {'name': 'Samsung Galaxy J3 2017 Zwart', 'Vodafone': '144', 'T-Mobile': '144', 'Tele2': '96', 'KPN': '144', 'Telfort': '144', 'Ben': '144', 'Hollandsnieuwe': '72', 'Lebara': '139,08'}, {'name': 'Samsung Galaxy Note9 512GB Midnight Zwart', 'Vodafone': '912', 'T-Mobile': '912', 'Tele2': '912', 'KPN': '864', 'Telfort': '960', 'Hollandsnieuwe': '960', 'Lebara': '955,16'}, {'name': 'Samsung Galaxy J4+ 32GB Goud', 'Vodafone': '144', 'T-Mobile': '144', 'Tele2': '96', 'KPN': '144', 'Telfort': '144', 'Ben': '144', 'Hollandsnieuwe': '144', 'Lebara': '144,41'}, {'name': 'Samsung Galaxy J5 2017 Goud', 'Vodafone': '144', 'T-Mobile': '168', 'Tele2': '120', 'KPN': '144', 'Telfort': '144', 'Ben': '156', 'Hollandsnieuwe': '96', 'Lebara': '177,27'}, {'name': 'Samsung Galaxy S9+ 256GB Midnight Zwart', 'Vodafone': '720', 'T-Mobile': '720', 'Tele2': '720', 'KPN': '744', 'Telfort': '768', 'Hollandsnieuwe': '768', 'Lebara': '765,35'}, {'name': 'Samsung Galaxy J6+ 32GB Grijs', 'Vodafone': '168', 'T-Mobile': '144', 'Tele2': '144', 'KPN': '120', 'Telfort': '192', 'Ben': '192', 'Hollandsnieuwe': '192', 'Lebara': '187,10'}, {'name': 'Samsung Galaxy J7 2017 Zwart', 'Vodafone': '192', 'T-Mobile': '192', 'Tele2': '144', 'KPN': '192', 'Telfort': '192', 'Ben': '180', 'Hollandsnieuwe': '192', 'Lebara': '182,04'}, {'name': 'Samsung Galaxy S9 256GB Midnight Zwart', 'Vodafone': '672', 'T-Mobile': '648', 'Tele2': '648', 'KPN': '696', 'Telfort': '696', 'Hollandsnieuwe': '696', 'Lebara': '696,67'}, {'name': 'Samsung Galaxy xCover 4 Zwart', 'Vodafone': '168', 'T-Mobile': '168', 'Tele2': '120', 'KPN': '168', 'Telfort': '168', 'Ben': '180', 'Hollandsnieuwe': '168', 'Lebara': '175,23'}, {'name': 'Samsung Galaxy J3 Goud', 'Vodafone': '120', 'T-Mobile': '120', 'Tele2': '72', 'KPN': '120', 'Telfort': '120', 'Ben': '120', 'Hollandsnieuwe': '120', 'Lebara': '120'}, {'name': 'Samsung Galaxy Note8 Zwart', 'Vodafone': '648', 'T-Mobile': '648', 'Tele2': '600', 'KPN': '648', 'Telfort': '648', 'Hollandsnieuwe': '648', 'Lebara': '647'}, {'name': 'Samsung Galaxy S8+ Zwart', 'Vodafone': '552', 'T-Mobile': '552', 'Tele2': '504', 'KPN': '552', 'Telfort': '552', 'Hollandsnieuwe': '552', 'Lebara': '547'}, {'name': 'Samsung Galaxy A3 2017 Zwart', 'Vodafone': '240', 'T-Mobile': '240', 'Tele2': '192', 'KPN': '240', 'Telfort': '240', 'Ben': '240', 'Hollandsnieuwe': '240', 'Lebara': '235,73'}, {'name': 'Samsung Galaxy A9 Lemonade Blauw', 'Vodafone': '480', 'T-Mobile': '456', 'Tele2': '432', 'KPN': '456', 'Telfort': '480', 'Ben': '492', 'Hollandsnieuwe': '480', 'Lebara': '491,47'}, {'name': 'Samsung Galaxy S7 Edge Blauw', 'Vodafone': '408', 'T-Mobile': '408', 'Tele2': '360', 'KPN': '408', 'Telfort': '408', 'Hollandsnieuwe': '408', 'Lebara': '399'}, {'name': 'Samsung Galaxy A3 2016 Goud', 'Vodafone': '192', 'T-Mobile': '192', 'Tele2': '144', 'KPN': '192', 'Telfort': '192', 'Ben': '204', 'Hollandsnieuwe': '192', 'Lebara': '199'}, {'name': 'Samsung Galaxy J7 2016 Goud', 'Vodafone': '264', 'T-Mobile': '264', 'Tele2': '216', 'KPN': '264', 'Telfort': '264', 'Ben': '252', 'Hollandsnieuwe': '264', 'Lebara': '257,25'}, {'name': 'Samsung Galaxy S6 32GB Goud', 'Vodafone': '288', 'T-Mobile': '288', 'Tele2': '240', 'KPN': '288', 'Telfort': '288', 'Ben': '288', 'Hollandsnieuwe': '288', 'Lebara': '288'}, {'name': 'Samsung Galaxy S6 Edge 32GB Goud', 'Vodafone': '384', 'T-Mobile': '384', 'Tele2': '336', 'KPN': '384', 'Telfort': '384', 'Ben': '396', 'Hollandsnieuwe': '384', 'Lebara': '394,97'}, {'name': 'Samsung Galaxy A5 2016 Goud', 'Vodafone': '312', 'T-Mobile': '312', 'Tele2': '264', 'KPN': '312', 'Telfort': '312', 'Ben': '312', 'Hollandsnieuwe': '312', 'Lebara': '311,15'}, {'name': 'Samsung Galaxy J5 2016 Goud', 'Vodafone': '192', 'T-Mobile': '192', 'Tele2': '144', 'KPN': '192', 'Telfort': '192', 'Ben': '180', 'Hollandsnieuwe': '192', 'Lebara': '181,92'}]
         create_page(phones_info)
         upload_page("Samsung.html")
-        sleep(60)
         driver.close()
+        sleep(60)
